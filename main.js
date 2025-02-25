a5fd37b9d48b84b1fd47a895d4a598c6f0055e78
Chari69 <jd.maceirasg@gmail.com>
2025-02-25 00:46:06 -0400

    mejorar la base antigua del proyecto

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 var sesion = false;
 
-function iniciarSesion() {
-    var form = document.getElementById("formLogin").submit();
+function iniciarSesion(event) {
+	event.preventDefault(); // Previene que la pagina se recargue.
 
-    console.log(form);
-    var usuario = form.getElementById("usuario");
-    var pass = form.getElementById("password");
+	var form = document.getElementById("formLogin");
 
-    if (usuario == "YOELMARICO69" && pass == "culo") {
-        alert("SESION INICIADA");
-    } 
-}  
+	console.log(form);
+	var usuario = document.getElementById("usuario").value;
+	var pass = document.getElementById("password").value;
 
+	if (usuario == "seller456" && pass == "Intro123") {
+		alert("SESION INICIADA " + usuario);
+	} else if (usuario == "dancabello" && pass == "J5*asdRD.s") {
+		alert("SESION INICIADA " + usuario);
+	} else if (usuario == "root" && pass == "dochouse") {
+		alert("SESION INICIADA " + usuario);
+	} else {
+		var noAutorizado = document.getElementById("error");
+		noAutorizado.innerHTML = "Usuario o contraseña incorrectas.";
+	}
+}
 
+document.getElementById("formLogin").addEventListener("submit", iniciarSesion);
