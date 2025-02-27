4639c7b55d63fd5b4d2236b32450c33d61058f6c
Chari69 <jd.maceirasg@gmail.com>
2025-02-26 23:50:17 -0400

    agregarProducto() y obtenerProducto() parcialmente agregados hacer form de registro, placeholders y otras mejoras

--- main.js
+++ main.js
@@ -11,14 +11,39 @@ function iniciarSesion(event) {
 
 	if (usuario == "seller456" && pass == "Intro123") {
 		alert("SESION INICIADA " + usuario);
+		window.location.href = "./panel_vendedor.html";
 	} else if (usuario == "dancabello" && pass == "J5*asdRD.s") {
 		alert("SESION INICIADA " + usuario);
+		window.location.href = "./panel_comprador.html";
 	} else if (usuario == "root" && pass == "dochouse") {
 		alert("SESION INICIADA " + usuario);
+		window.location.href = "./panel_administrador.html";
 	} else {
 		var noAutorizado = document.getElementById("error");
 		noAutorizado.innerHTML = "Usuario o contraseña incorrectas.";
 	}
+}
+
+// Obtener el listado de producots
+function agregarProducto() {
+	const producto = [
+		{
+			id: 1,
+			nombre: "Laptop",
+			descripcion: "Del gobierno",
+			precio: 69.69,
+			img: "./res/cat.jpg",
+			cantidad: 10,
+		},
+	];
+
+	// Guardar el producto en localStorage
+	localStorage.setItem("producto", JSON.stringify(producto));
+}
+
+function obtenerProducto(variable) {
+	const productoGuardado = JSON.parse(localStorage.getItem("producto"));
+	console.log(productoGuardado[variable]);
 }
 
 document.getElementById("formLogin").addEventListener("submit", iniciarSesion);
